34161207ef56df8720afcb0f88589490958345ff
Rodrigo Prata <rodrigo.prata@dafiti.com.br>
2018-03-31 16:15:11 -0300

    Tabs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let port = 3001
 // static route
 app.use(express.static(__dirname + "/public"));
 
-app.use("/", require("./app/routes/default"));
+app.use("*", require("./app/routes/default"));
 
 app.listen(port, () => {
     console.log(`server running at port ${port}`)
