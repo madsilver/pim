28dc07ccc4c48d680dd4573e082df622d56f65a8
Rodrigo Prata <rodrigo.prata@dafiti.com.br>
2018-04-08 16:54:53 -0300

    Toast

--- app/model/settings.js
+++ app/model/settings.js
@@ -1,19 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// var SettingsSchema = new Schema({
-//     name: { type: String, required: true },
-//     description: { type: String },
-//     value: { type: String, required: true }
-// },{
-//     versionKey: false
-// });
-
 var SettingsSchema = new Schema({
-    url: {
-        title: String,
-        value: String
-    }
+    apiUrl: { type: String, required: true },
+    company: { type: String }
 },{
     versionKey: false
 });
